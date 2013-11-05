d36b6ca3be7ffd0aaddc33b1511f6ea09b8f292d
Juan C Salazar <jcsalazarm@gmail.com>
2013-11-04 23:43:00 -0500

    se fue el segundo commit

---
+++ myWords/clases/clPalabras.h
@@ -0,0 +1,9 @@
+
+#import <Foundation/Foundation.h>
+
+@interface clPalabras : NSObject
+
+@property (nonatomic, strong) NSString * palabra;
+@property (nonatomic, strong) NSString * dsPalabra;
+
+@end
